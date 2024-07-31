559f7dce69979b5f3123432693895325ec5219b4
furlanjamily <102761258+furlanjamily@users.noreply.github.com>
2024-07-31 19:32:39 -0300

    pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,16 +1,17 @@
 import { CheckCircle2 } from "lucide-react";
 import { pricingOptions } from "../constants";
 
+
 const Pricing = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wider">
         Planos disponíveis
       </h2>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap h-max">
         {pricingOptions.map((option, index) => (
           <div key={index} className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 p-2 md:justify-center md:items-center ">
-            <div className="p-10 border border-neutral-700 rounded-xl">
+            <div className="pricing-card p-10 border border-neutral-700 rounded-xl">
               <p className="text-4xl mb-8">
                 {option.title}
                 {option.title === "Pro" && (
